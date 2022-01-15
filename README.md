313df38d0b8dbb2f7b2d6ea72a403e7dc726827f
luis antonio <yepesberbesi@gmail.com>
2022-01-15 10:39:59 -0500

    este es mi reame.md de mi proyecto

--- README.md
+++ README.md
@@ -10,3 +10,12 @@
 en javascript
 -tercer paso: crear funciones
 -cuarto paso: integrar js con HTML
+
+
+## taller #2: porcentajes y descuentos
+
+- primer paso: definir las formulas
+-segundo paso: inplementar la formulas 
+en javascript
+-tercer paso: crear funciones
+-cuarto paso: integrar js con HTML
--- figuras.js
+++ figuras.js
@@ -1,18 +1,14 @@
 // codigo del cuadrado
 console.group("cuadrados");
 
-/* const ladocuadrado = 5;
-console.log("los lados del cuadrado miden: " + ladocuadrado + "cm"); */
 
 function perimetrocuadrado(lado) {
     return lado * 4;
 }
-/* console.log("el perimetro del cuadrado es : " + perimetrocuadrado + "cm"); */
 
 function areacuadrado(lado) {
     return lado * lado;
 }
-/* console.log("el area del cuadrado es : " + perimetrocuadrado + "cmˆ2"); */
 console.groupEnd();
 
 
@@ -24,48 +20,25 @@ const basetriangulo = 4;
 
 
 
-/* console.log(
-    "los lados del triangulo miden: " 
-    + ladotriangulo1 
-    + "cm, " 
-    + ladotriangulo2 
-    + "cm," 
-    + basetriangulo 
-    + "cm"
-    ); */
 
-   /*  const alturatriangulo = 5.5;
-    console.log("la altura del triangulo es de: "  + alturatriangulo + "cm");
- */
     function perimetrotriangulo(lado1, lado2, base) {
         return lado1 + lado2 + base;
     }
-    /* console.log("el perimetro del triangulo es: " + perimetrotriangulo + "cm"); */
-
     
     function areatriangulo(base, altura) {
         return (base * altura) / 2;
     }
-    /* console.log("el area del triangulo es : " + areatriangulo + "cmˆ2"); */
     
     console.groupEnd();
 
     // codigo del circulo
     console.group("circulos");
 
-    // radio
-   /*  const radiocirculo = 4;
-    console.log("el radio del circulo es: "  + radiocirculo + "cm"); */
+   
 
     function diametrocirculo(radio) {
         return radio * 2;
     }
-
-    // diametro
-  
-
-    // pi
-  
 
     // circunferencia
 
@@ -82,3 +55,26 @@ const basetriangulo = 4;
     }
 
     console.groupEnd();
+
+    // aqui interactuamos con el html
+
+    function calcularperimetrocuadrado() {
+        const input = document.getElementById("inputcuadrado");
+        const value = input.value;
+
+        const perimetro = perimetrocuadrado(value);
+        alert(perimetro);
+    }
+
+    function calcularareacuadrado() {
+        const input = document.getElementById("inputcuadrado");
+        const value = input.value;
+
+        const area = areacuadrado(value);
+        alert(area);
+
+    }
+
+    // triangulo
+
+    
